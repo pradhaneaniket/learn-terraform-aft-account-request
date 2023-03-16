507008ceb0c8806286e81e6a94a934da391a876d
unknown <aniket.pradhane@blazeclan.com>
2023-03-16 18:21:48 +0530

    MasterAccountUserConfig

---
+++ terraform/account_requet_masteraccount.tf
@@ -0,0 +1,25 @@
+module "sharedService" {
+
+    source = "./modules/aft-account-request"
+    control_tower_parameters = {
+        AccountEmail = "birajdar007shivraj@gmail.com"
+        AccountName = "gmail.com"
+        ManagedOrganizationalUnit = "root"
+        SSOUserEmail = "aniket.pradhane@blazeclan.com"
+        SSOUserFirstName = "Aniket"
+        SSOUserLastName = "Pradhane"
+
+    }
+
+    account_tags = {
+        "Test:Account"       = "MasterAccount"
+        "Test:Vended"        = "True"
+    }
+
+    change_management_parameters = {
+        change_reason = "Initial Account provision"
+        change_requested_by = "Aniket Pradhane"
+    
+    }
+    account_customizations_name = "masteraccount"
+}
