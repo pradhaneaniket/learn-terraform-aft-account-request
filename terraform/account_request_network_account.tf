b1dc6470f7c7fbb7da2d7590dfaafaf9dcac6e33
unknown <aniket.pradhane@blazeclan.com>
2023-03-23 17:24:52 +0530

    Network Account Vending

---
+++ terraform/account_request_network_account.tf
@@ -0,0 +1,25 @@
+module "sharedService" {
+
+    source = "./modules/aft-account-request"
+    control_tower_parameters = {
+        AccountEmail = "ahtesham.sheikh+network@blazecla.com"
+        AccountName = "Network"
+        ManagedOrganizationalUnit = "Infrastructure"
+        SSOUserEmail = "aniket.pradhane@blazeclan.com"
+        SSOUserFirstName = "Aniket"
+        SSOUserLastName = "Pradhane"
+
+    }
+
+    account_tags = {
+        "Test:Account"       = "NetworkAccount"
+        "Test:Vended"        = "True"
+    }
+
+    change_management_parameters = {
+        change_reason = "Initial Account provision"
+        change_requested_by = "Aniket Pradhane"
+    
+    }
+    account_customizations_name = "networkaccount"
+}
